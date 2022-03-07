19ff369896839ba22ffaf1e432e85b8bca427d15
Mark Messner <mark.a.messner@gmail.com>
2022-03-07 14:26:42 +1100

    (Bad) styling of product-item component

--- CMCShoppingCart.UI/src/app/common.module.ts
+++ CMCShoppingCart.UI/src/app/common.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon'
+import { MatInputModule } from '@angular/material/input'
+
 
 @NgModule({
     exports: [
         BrowserModule,
         FlexLayoutModule,
+        FormsModule,
         MatButtonModule,
-        MatCardModule
+        MatCardModule,
+        MatIconModule,
+        MatInputModule,
+        ReactiveFormsModule
     ],
   })
   export class CommonModule { }
--- CMCShoppingCart.UI/src/app/products/product-item.component.ts
+++ CMCShoppingCart.UI/src/app/products/product-item.component.ts
@@ -1,10 +1,39 @@
 import { Component, Input } from '@angular/core';
+import { AbstractControl, FormControl, ValidationErrors } from '@angular/forms';
 import { Product } from './product.models';
 
 @Component({    
     selector: 'product-item',
+    styleUrls: ['./product-item.component.scss'],
     templateUrl: './product-item.component.html'
 })
 export class ProductItemComponent{
     @Input() product: Product|undefined;
+    readonly quantityControl = new FormControl(1, this.quantityControlValidator);
+    isBuying = false;
+
+    onInitialBuy(): void{
+        this.isBuying = true;
+    }
+
+    onBuyCancel(): void{
+        this.quantityControl.setValue(1);
+        this.isBuying = false;
+    }
+
+    onBuy(): void{
+        this.isBuying = false;
+        this.quantityControl.setValue(1);
+    }
+
+    private quantityControlValidator(control: AbstractControl): ValidationErrors | null{
+        const message = 'Value muse be between 1 & 1000';
+        const formControl = control as FormControl;
+        const value = formControl.value;
+        const isNan = isNaN(value);
+        if (isNan) return { quantity: message };
+        const number = Number(value);
+        const result = number >= 1 && number <= 1000 ? null : { quantity: message };
+        return result;
+    }
 }
